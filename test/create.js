b7e87af8e604a8f45f5d15ce049d1b3f3e60eb96
James Halliday <mail@substack.net>
2015-12-23 14:51:02 -0500

    failing create test, way not in the results

---
+++ test/create.js
@@ -0,0 +1,61 @@
+var test = require('tape')
+var hyperlog = require('hyperlog')
+var fdstore = require('fd-chunk-store')
+var path = require('path')
+var memdb = require('memdb')
+
+var tmpdir = require('os').tmpdir()
+var storefile = path.join(tmpdir, 'osm-store-' + Math.random())
+
+var osmdb = require('../')
+
+test('create 3 nodes and a way', function (t) {
+  t.plan(6)
+  var osm = osmdb({
+    log: hyperlog(memdb(), { valueEncoding: 'json' }),
+    db: memdb(),
+    store: fdstore(4096, storefile),
+    size: 4096
+  })
+  var docs = {
+    A: { type: 'node', loc: [ 64.5, -147.3 ] },
+    B: { type: 'node', loc: [ 63.9, -147.6 ] },
+    C: { type: 'node', loc: [ 64.2, -146.5 ] },
+    D: { type: 'way', refs: [ 'A', 'B', 'C' ] }
+  }
+  var names = {}
+  var nodes = {}
+
+  var keys = Object.keys(docs).sort()
+  ;(function next () {
+    if (keys.length === 0) return ready()
+    var key = keys.shift()
+    var doc = docs[key]
+    if (doc.refs) {
+      doc.refs = doc.refs.map(function (ref) { return names[ref] })
+    }
+    osm.create(doc, function (err, k, node) {
+      t.ifError(err)
+      names[key] = k
+      nodes[k] = node
+      next()
+    })
+  })()
+
+  function ready () {
+    var q0 = [[63,65],[-148,-146]]
+    osm.query(q0, function (err, res) {
+      t.ifError(err)
+      t.deepEqual(res.sort(idcmp), [
+        { type: 'node', loc: [ 64.5, -147.3 ], id: names.A },
+        { type: 'node', loc: [ 63.9, -147.6 ], id: names.B },
+        { type: 'node', loc: [ 64.2, -146.5 ], id: names.C },
+        { type: 'way', refs: [ names.A, names.B, names.C ], id: names.D }
+      ].sort(idcmp))
+    })
+  }
+})
+
+function idcmp (a, b) {
+  return a.id < b.id ? -1 : 1
+}
