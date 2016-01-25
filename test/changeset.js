d4e42c1a28ad30f91ad86f7931ba6c3733d0b7d1
James Halliday <mail@substack.net>
2016-01-24 20:35:03 -0800

    changeset test

--- test/changeset.js
+++ test/changeset.js
@@ -11,7 +11,7 @@ var storefile = path.join(tmpdir, 'osm-store-' + Math.random())
 var osmdb = require('../')
 
 test('changeset', function (t) {
-  t.plan(16)
+  t.plan(15)
   var osm = osmdb({
     log: hyperlog(memdb(), { valueEncoding: 'json' }),
     db: memdb(),
@@ -23,7 +23,9 @@ test('changeset', function (t) {
     B: { type: 'node', lat: 64.5, lon: -147.3, changeset: 'A' },
     C: { type: 'node', lat: 63.9, lon: -147.6, changeset: 'A' },
     D: { type: 'node', lat: 64.2, lon: -146.5, changeset: 'A' },
-    E: { type: 'way', refs: [ 'B', 'C', 'D' ], changeset: 'A' }
+    E: { type: 'way', refs: [ 'B', 'C', 'D' ], changeset: 'A' },
+    F: { type: 'changeset', tags: { comment: 'blah' } },
+    G: { type: 'node', lat: 64.2, lon: -146.5, changeset: 'F' }
   }
   var names = {}
   var nodes = {}
@@ -50,49 +52,23 @@ test('changeset', function (t) {
   })()
 
   function ready () {
-    var q0 = [[63,65],[-148,-146]]
-    var ex0 = [
-      { type: 'node', lat: 64.5, lon: -147.3,
-        id: names.B, version: versions.B, changeset: names.A },
-      { type: 'node', lat: 63.9, lon: -147.6,
-        id: names.C, version: versions.C, changeset: names.A },
-      { type: 'node', lat: 64.2, lon: -146.5,
-        id: names.D, version: versions.D, changeset: names.A },
-      { type: 'way', refs: [ names.B, names.C, names.D ],
-        id: names.E, version: versions.E, changeset: names.A }
-    ].sort(idcmp)
-    osm.query(q0, function (err, res) {
+    osm.changes(names.A, function (err, keys) {
       t.ifError(err)
-      t.deepEqual(res.sort(idcmp), ex0, 'full coverage query')
+      var expected = [ versions.B, versions.C, versions.D, versions.E ]
+      t.deepEqual(keys, expected.sort())
     })
-    collect(osm.queryStream(q0), function (err, res) {
+    osm.changes(names.F, function (err, keys) {
       t.ifError(err)
-      t.deepEqual(res.sort(idcmp), ex0, 'full coverage stream')
+      var expected = [ versions.G ]
+      t.deepEqual(keys, expected.sort())
     })
-    var q1 = [[62,64],[-149.5,-147.5]]
-    var ex1 = [
-      { type: 'node', lat: 63.9, lon: -147.6,
-        id: names.C, version: versions.C, changeset: names.A },
-      { type: 'way', refs: [ names.B, names.C, names.D ],
-        id: names.E, version: versions.E, changeset: names.A }
-    ].sort(idcmp)
-    osm.query(q1, function (err, res) {
+    osm.get(names.A, function (err, doc) {
       t.ifError(err)
-      t.deepEqual(res.sort(idcmp), ex1, 'partial coverage query')
+      t.equal(doc[Object.keys(doc)[0]].tags.comment, 'whatever')
     })
-    collect(osm.queryStream(q1), function (err, res) {
+    osm.get(names.F, function (err, doc) {
       t.ifError(err)
-      t.deepEqual(res.sort(idcmp), ex1, 'partial coverage stream')
-    })
-    var q2 = [[62,64],[-147,-145]]
-    var ex2 = []
-    osm.query(q2, function (err, res) {
-      t.ifError(err)
-      t.deepEqual(res.sort(idcmp), ex2, 'empty coverage query')
-    })
-    collect(osm.queryStream(q2), function (err, res) {
-      t.ifError(err)
-      t.deepEqual(res.sort(idcmp), ex2, 'empty coverage stream')
+      t.equal(doc[Object.keys(doc)[0]].tags.comment, 'blah')
     })
   }
 })
