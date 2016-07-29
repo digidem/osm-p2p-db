a67ec2cf67b54afe47036cc39ee599c4a751ff22
James Halliday <mail@substack.net>
2016-07-28 23:10:03 -0700

    failing type order test

---
+++ test/ordered.js
@@ -0,0 +1,67 @@
+var test = require('tape')
+var hyperlog = require('hyperlog')
+var fdstore = require('fd-chunk-store')
+var path = require('path')
+var memdb = require('memdb')
+var collect = require('collect-stream')
+
+var tmpdir = require('os').tmpdir()
+var storefile = path.join(tmpdir, 'osm-store-' + Math.random())
+
+var osmdb = require('../')
+
+test('ordered types', function (t) {
+  t.plan(4)
+  var osm = osmdb({
+    log: hyperlog(memdb(), { valueEncoding: 'json' }),
+    db: memdb(),
+    store: fdstore(4096, storefile)
+  })
+  var rows = [
+    { type: 'put', key: 'A', value: { type: 'node', lat: 64.5, lon: -147.3 } },
+    { type: 'put', key: 'B', value: { type: 'node', lat: 63.9, lon: -147.6 } },
+    { type: 'put', key: 'C', value: { type: 'node', lat: 64.2, lon: -146.5 } },
+    { type: 'put', key: 'D', value: { type: 'way', refs: [ 'A', 'B', 'C' ] } },
+    { type: 'put', key: 'E', value: { type: 'node', lat: 64.5, lon: -147.3 } },
+    { type: 'put', key: 'F', value: { type: 'node', lat: 65.1, lon: -148.5 } },
+    { type: 'put', key: 'G', value: { type: 'way', refs: [ 'E', 'F' ] } }
+  ]
+  osm.batch(rows, function (err, nodes) {
+    t.error(err)
+    var q0 = [[63,65],[-148,-146]]
+    var ext0 = [ 'node', 'node', 'node', 'node', 'node', 'way', 'way' ]
+    var ex0 = [
+      { type: 'node', lat: 64.5, lon: -147.3,
+        id: 'A', version: nodes[0].key },
+      { type: 'node', lat: 63.9, lon: -147.6,
+        id: 'B', version: nodes[1].key },
+      { type: 'node', lat: 64.2, lon: -146.5,
+        id: 'C', version: nodes[2].key },
+      { type: 'node', lat: 64.5, lon: -147.3,
+        id: 'E', version: nodes[4].key },
+      { type: 'node', lat: 65.1, lon: -148.5,
+        id: 'F', version: nodes[5].key },
+      { type: 'way', refs: [ 'A', 'B', 'C' ],
+        id: 'D', version: nodes[3].key },
+      { type: 'way', refs: [ 'E', 'F' ],
+        id: 'G', version: nodes[6].key }
+    ].sort(idcmp)
+
+    osm.query(q0, { order: 'type' }, function (err, res) {
+      t.error(err)
+      t.deepEqual(res.sort(idcmp), ex0, 'results')
+      t.deepEqual(res.map(type), ext0, 'types')
+    })
+  })
+})
+
+function type (x) { return x.type }
+function idcmp (a, b) {
+  return a.type === b.type
+    ? cmpt(a.type, b.type)
+    : (a.id < b.id ? -1 : 1)
+}
+var types = ['node','way','relation']
+function cmpt (a, b) {
+  return types.indexOf(a) - types.indexOf(b)
+}
