9706fa0690b2efb35d935f1faa5cfb90389c783c
Stephen Whitmore <sww@eight45.net>
2017-04-22 15:25:27 -1000

    Add skipped regression test for relation/query bug

--- test/relation.js
+++ test/relation.js
@@ -87,6 +87,72 @@ test('relation of ways', function (t) {
   }
 })
 
+test.skip('REGRESSION: multiple nodes referred to by a relation', function (t) {
+  var docs = {
+    A: { type: 'node', lat: 64.5, lon: -147.3 },
+    B: { type: 'node', lat: 63.9, lon: -147.6 },
+    C: { type: 'relation', members: [
+      { type: 'node', ref: 'A' },
+      { type: 'node', ref: 'B' }
+    ] }
+  }
+  var keys = Object.keys(docs).sort()
+  t.plan(keys.length + 4)
+
+  var osm = osmdb({
+    log: hyperlog(memdb(), { valueEncoding: 'json' }),
+    db: memdb(),
+    store: fdstore(4096, storefile)
+  })
+
+  var names = {}
+  var nodes = {}
+  var versions = {}
+
+  ;(function next () {
+    if (keys.length === 0) return ready()
+    var key = keys.shift()
+    var doc = docs[key]
+    if (doc.refs) {
+      doc.refs = doc.refs.map(function (ref) { return names[ref] })
+    }
+    ;(doc.members || []).forEach(function (m) {
+      if (m.ref) m.ref = names[m.ref]
+    })
+    osm.create(doc, function (err, k, node) {
+      t.ifError(err)
+      names[key] = k
+      versions[key] = node.key
+      nodes[k] = node
+      next()
+    })
+  })()
+
+  function ready () {
+    var q0 = [[-90,90],[-180,180]]
+    var ex0 = [
+      { type: 'node', lat: 62.1, lon: -145.1,
+        id: names.A, version: versions.A },
+      { type: 'node', lat: 62.3, lon: -146.4,
+        id: names.B, version: versions.B },
+      { type: 'relation',
+        members: [
+          { type: 'node', ref: names.A },
+          { type: 'node', ref: names.B }
+        ],
+        id: names.C, version: versions.C }
+    ].sort(idcmp)
+    osm.query(q0, function (err, res) {
+      t.ifError(err)
+      t.deepEqual(res.sort(idcmp), ex0, 'relation of ways')
+    })
+    collect(osm.queryStream(q0), function (err, res) {
+      t.ifError(err)
+      t.deepEqual(res.sort(idcmp), ex0, 'relation of ways stream')
+    })
+  }
+})
+
 function idcmp (a, b) {
   return a.id < b.id ? -1 : 1
 }
