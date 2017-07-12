4d7b00b91f1aa512367dfbeb80541287588a4af6
Stephen Whitmore <sww@eight45.net>
2017-07-11 17:22:27 -0700

    test: use create_db for forked_joins

--- test/forked_joins.js
+++ test/forked_joins.js
@@ -1,24 +1,13 @@
 var test = require('tape')
-var hyperlog = require('hyperlog')
-var fdstore = require('fd-chunk-store')
-var path = require('path')
-var memdb = require('memdb')
-var collect = require('collect-stream')
+var makeOsm = require('./create_db')
 
-var tmpdir = require('os').tmpdir()
-var storefile = path.join(tmpdir, 'osm-store-' + Math.random())
-
-var osmdb = require('../')
 var versions = {}
 var ways = []
-var osm = osmdb({
-  log: hyperlog(memdb(), { valueEncoding: 'json' }),
-  db: memdb(),
-  store: fdstore(4096, storefile)
-})
+var osm
 
 test('setup db', function (t) {
   t.plan(9)
+  osm = makeOsm()
   var batch = [
     { type: 'put', key: 'A', value: { type: 'node', lat: 64.5, lon: -147.3 } },
     { type: 'put', key: 'B', value: { type: 'node', lat: 1.0, lon: 2.0 } },
