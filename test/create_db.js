c77134c6b1edc9d07f48d9af5a4bba80d4ef59a2
Stephen Whitmore <sww@eight45.net>
2017-07-17 12:19:14 -0700

    test: use in-memory chunk store for tests
    Tests now run entirely in memory without any filesystem mutations! <3

--- test/create_db.js
+++ test/create_db.js
@@ -1,17 +1,13 @@
 var hyperlog = require('hyperlog')
-var fdstore = require('fd-chunk-store')
-var path = require('path')
+var memstore = require('memory-chunk-store')
 var memdb = require('memdb')
-var tmpdir = require('os').tmpdir()
 
 module.exports = function () {
-  var storefile = path.join(tmpdir, 'osm-store-' + Math.random())
-
   var osmdb = require('../')
 
   return osmdb({
     log: hyperlog(memdb(), { valueEncoding: 'json' }),
     db: memdb(),
-    store: fdstore(4096, storefile)
+    store: memstore(4096)
   })
 }
